0a5a5376000c16353352751a36e5808f0edc0c9c
nyanta22 <yuyuyuyuto.nyanta@gmail.com>
2019-03-02 15:56:35 +0900

    Edit message.js

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,39 @@
+$(function(){
+  function buildHTML(message){
+    var html = `<div class="message latest">
+                  <div class="user__name">
+                    ${message.name}
+                  </div>
+                  <div class="message__time">
+                    ${message.created_at}
+                  </div>
+                    <p class="message__content">
+                      ${message.content}
+                    </p>
+                  </div>`
+    return html;
+  }
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var href = window.location.href
+    $.ajax({
+      url: href,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html)
+      $('.Chat__message').val('')
+      $('.Chat__send').prop('disabled',false);
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight});
+    })
+    .fail(function() {
+      alert('error');
+    })
+  });
+});
